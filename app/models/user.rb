eeaed2986693c361acf07fd51fdc688e138badb1
Tetsu Yokoyama <t.y.develop.1021@gmail.com>
2018-07-27 13:34:36 +0900

    validates change

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,12 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   # has_one :profiles
 
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
-  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 
-  validates :email, uniqueness: true
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 end
